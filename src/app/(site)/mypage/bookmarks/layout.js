1d26425eeff25d2b679c989d1ad1504ecac91259
jema <smh1141@naver.com>
2026-08-17 19:15:42 +0900

    feat: 다양한 페이지 메타데이터 추가 및 레이아웃 구성 개선

---
+++ src/app/(site)/mypage/bookmarks/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "내 북마크 | 프다!",
+  description: "북마크한 요약 노트를 모아보세요.",
+  robots: { index: false, follow: false },
+};
+
+export default function MyBookmarksLayout({ children }) {
+  return children;
+}
